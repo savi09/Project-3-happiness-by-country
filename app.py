b6a5adcad65fe93559bdc9e0e308e0a842cd6b7a
jennifermarie6sl <85765364+jennifermarie6sl@users.noreply.github.com>
2021-10-18 21:00:31 -0700

    Route working containing all data

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-# From web scraping activities day 3, #8
 from flask import Flask, render_template, jsonify
 from flask_pymongo import PyMongo
 
@@ -6,31 +5,19 @@ app = Flask(__name__)
 
 mongo = PyMongo(app, uri="mongodb://localhost:27017/happy_db")
 
-# setup mongo connection
-# conn = "mongodb://localhost:27017"
-# client = pymongo.MongoClient(conn)
- 
-# connect to mongo db and collection
-# db = client.happy_db
-# happy = mongo.db.happy
-# ss_db = mongo.db.suicide
+@app.route('/')
+def db_ping():
 
-
-@app.route('/data/<year>')
-def data_find(year):
-    # write a statement that finds all the items in the db and sets it to a variable
-    happy_list = mongo.db.happy.find({'year': year}, {'_id': False})
-    suicide_list = mongo.db.suicide.find({'year': year}, {'_id': False})
-    print(happy_list)
-
-    data = {'happiness': happy_list, 'suicide': suicide_list}
-    parsed = [x for x in data]
-    print('parsed: ', parsed)
-    return jsonify(parsed)
+    return render_template('index.html', data = 'Happy?')
     
-    # render an index.html template and pass it the data you retrieved from the database
-    return render_template('index.html', data = data)
+@app.route('/data')
+def db_data():
 
+    db_data = mongo.db.happy.find({}, {'_id': False, 'standard_error': False})
+    ss_data = mongo.db.suicide.find({}, {'_id': False, 'hdi_for_year': False})
+    data = {'happiness': [x for x in db_data], 'suicide': [x for x in ss_data]}
+    print('this route was pinged')
+    return jsonify(data)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
---
+++ static/js/app.js
@@ -0,0 +1,10 @@
+d3.json('/data').then(function (myData) {
+    console.log("Year")
+    console.log(myData)
+    // plotly here 1 chart chart that is static, then add interactions
+});
+
+// d3.json(`/ss_data/Albania`).then(function (mySSData) {
+//     console.log("SS Data")
+//     console.log(mySSData)
+// });
